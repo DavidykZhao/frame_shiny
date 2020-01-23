5ad756de66ebaf21d48c0e13c54eace38722387d
Yikai Zhao <zdwzhaoyikai@gmail.com>
2020-01-23 15:02:26 -0600

    added tabbox button for stage 1

---
+++ frame_shiny_app/simulation_app/ui.R
@@ -0,0 +1,97 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+#install.packages('shinydashboard')
+library(shinydashboard)
+
+
+
+header <- dashboardHeader(
+    title = 'Visualization of model performances',
+    titleWidth = 350
+    
+    )
+
+
+
+
+
+
+sidebar <- dashboardSidebar(
+    
+    sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Introduction", icon = icon("info-circle"), tabName = "Introduction",
+                 badgeLabel = "new", badgeColor = "green")
+        )
+    )
+
+
+
+
+
+########################################################
+# tabBox for stage 1 - model performances across datasets
+tb_stage1 = tabBox(
+        title = "Datasets",
+        # The id lets us use input$tabset1 on the server to find the current tab
+        id = "tabset1", 
+        height = "600px",
+        width = 12, 
+    tabPanel("AG News",
+             fluidRow(             actionButton("go", "View the plot")
+                                   )
+             ),
+
+    tabPanel("DBPedia",
+              "Tab content 2"),
+    tabPanel("Yelp",
+             "Tab content 2"),
+    tabPanel("Amazon",
+             "Tab content 2"))
+
+# tabPanel("Customer", 
+#          "Tab content 2"),
+# ),
+########################################################
+
+
+
+body <- dashboardBody(
+    tabItems(
+        tabItem(tabName = "Introduction",
+                h2("Performances of models on a certain dataset"),
+                fluidRow(
+                    tb_stage1
+                ) # closing bracket for the fluidRow
+        )
+        
+      ), # closing bracket for tabItems
+    
+    h2("Performances a model on across datasets")
+    
+    
+    
+    
+    
+    
+) # closing bracket for dashboardBody obj
+
+
+
+
+
+
+
+
+dashboardPage(header, sidebar, body)
+
+
